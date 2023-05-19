efe3b14057875638a78ff576979b4b266a36df4f
brucejayasinghe <brucejay43@gmail.com>
2023-05-19 08:48:26 +0100

    Refactor test

---
+++ test/OddsGame.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "forge-std/Vm.sol";
+
+import "./utils/Utils.sol";
+
+contract OddsGameTest is Test {
+
+    
+
+}
+
